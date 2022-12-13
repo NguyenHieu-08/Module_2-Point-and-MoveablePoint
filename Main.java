95873690d51eaaa994888bcd7b602a54de0f1f5e
Hiếu Nguyễn <hieunghuu08@gmail.com>
2022-12-13 11:09:43 +0700

    Upload point and moveablepoint

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Point p = new Point();
+        System.out.println(p.toString());
+
+        MoveablePoint mp = new MoveablePoint();
+        mp.setSpeed(10.5f,10.5f);
+        System.out.println(mp.toString());
+        System.out.println(mp.move());
+    }
+}
